87df8aabd8abd4b4bb0b39a17677d8f6e47fb664
Alexis Beaulieu <marklermer.14@gmail.com>
2019-03-20 23:35:24 -0600

    Assignment three

--- platforms/browser/www/JS/scripts.js
+++ platforms/browser/www/JS/scripts.js
@@ -12,22 +12,39 @@ window.onload = function () {
     init(); // use for testing on chrome
        function init() {
         console.log("we are a go");
-        console.log(navigator)
+        console.log(document.getElementById('result'))
         document.getElementById('camButton').addEventListener('click',takePic);
         
         
     }
-    // var options = {
-    //     quality: 100,
-    //     sourceType: camera.PictureSourceType.PHOTOLIBRARY,
-    //     encodingType: camera.EncodingType.PNG,
-    //     cameraDirection: camera.Direction.FRONT
-    //     };
+    var options = {
+        quality: 50,
+        destinationType: Camera.DestinationType.FILE_URI,
+        // In this app, dynamically set the picture source, Camera or photo gallery
+        sourceType: Camera.PictureSourceType.CAMERA,
+        encodingType: Camera.EncodingType.JPEG,
+        mediaType: Camera.MediaType.PICTURE,
+        };
 
     function takePic() {
         console.log("first success!");
         navigator.camera.getPicture(success,error, options);
+        document.getElementById('overId').style.opacity = 0;
+        
         } 
+
+        function success(imageData) {
+             document.getElementById('result').src = imageData;
+            document.getElementById('camButton').innerHTML = "Snap Another Moment";
+            
+            setTimeout(() => {
+                document.getElementById('overId').style.opacity = 1;
+            },5000)
+        }
+
+        function error(message) {
+            alert("oh no the camera failed because" + message);
+        }
     
     };
     
